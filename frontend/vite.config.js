e4994cf04c3163629cb9a40e42103cd941e385aa
ac <mugishatumusifuchretien@gmail.com>
2026-05-03 07:15:28 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://identitycloud-1eyb.onrender.com',
         changeOrigin: true
       }
     }
